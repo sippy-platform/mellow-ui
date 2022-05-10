af1bb05991fe93ab4a4296df2cc5b9ecfa113579
Yannick <yannick.franssen@outlook.com>
2022-05-10 14:43:08 +0200

    Properly export TypeScript definitions

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import * as path from 'path';
+import dts from 'vite-plugin-dts';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -19,5 +20,5 @@ export default defineConfig({
       }
     }
   },
-  plugins: [react()]
+  plugins: [react(), dts()]
 })
